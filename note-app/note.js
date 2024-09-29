21c8c515cce7f583b2e20f81e9dd6fcc1817ca0f
Anasgour1 <159617159+Anasgour1@users.noreply.github.com>
2024-09-29 20:12:18 +0530

    Add files via upload

---
+++ note-app/note.js
@@ -0,0 +1,45 @@
+const noteContainer = document.querySelector(".notes-container");
+const creatBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes(){
+    noteContainer.innerHTML = localStorage.getItem("notes");
+
+}
+showNotes();
+
+function UpdateStorage(){
+    localStorage.setItem("notes", noteContainer.innerHTML);
+}
+
+
+creatBtn.addEventListener("click", ()=>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "delete.png";
+    noteContainer.appendChild(inputBox).appendChild(img)
+})
+
+noteContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        UpdateStorage();
+    }
+    else if(e.target.tagName === "p"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                UpdateStorage();
+            }
+        })
+    }
+})
+
+document.addEventListener("keydown", event => {
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
